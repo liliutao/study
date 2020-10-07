225701b84db986c97225d39332dae75f25917c58
liliutao <2322817200@qq.com>
2020-10-07 17:38:03 +0800

    commit7

---
+++ src/router/index.js
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+import vueRouter from 'vue-router'
+Vue.use(vueRouter)
+const router = new vueRouter({
+    mode: 'history',
+    routes: [{
+            path: '/',
+            redirect: '/index'
+        },
+        {
+            path: '/index',
+            component: () =>
+                import ('../views/index.vue')
+        },
+        {
+            path: '/cart',
+            component: () =>
+                import ('../views/cart.vue')
+        },
+        {
+            path: '/mine',
+            component: () =>
+                import ('../views/mine.vue')
+        },
+        {
+            path: '/details',
+            name: 'detail',
+            component: () =>
+                import ("../views/details.vue")
+
+        },
+        {
+            path: '/CartOrderFrom',
+            component: () =>
+                import ('../views/CartOrderFrom.vue')
+        },
+    ],
+    linkActiveClass: 'active'
+
+})
+export default router
